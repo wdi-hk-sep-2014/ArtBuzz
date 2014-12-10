3bb7ca755e8ab3b4474d1969288647bd74bdfb72
ashtoncoghlan <ashton.coghlan@gmail.com>
2014-12-10 12:24:17 +0800

    Moved text from controller to view. Removed @content variable.

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,7 +1,6 @@
 class EventController < ApplicationController
 
   def index
-    @content_events = "Below is a list of all events"
     @events = Event.all
   end
 
--- app/controllers/venue_controller.rb
+++ app/controllers/venue_controller.rb
@@ -1,8 +1,6 @@
 class VenueController < ApplicationController
 
   def index
-# put text in view
-    @content_venues = "Below is a list of all venues in Hong Kong"
     @venues = Venue.all
   end
 
